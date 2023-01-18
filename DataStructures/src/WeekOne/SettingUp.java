244bae18e404bbf24067b4d6ba688592de9e7223
Christian <Christian@DESKTOP-G9A6H09.myfiosgateway.com>
2023-01-18 18:02:30 -0500

    Initial Project Commit

---
+++ DataStructures/src/WeekOne/SettingUp.java
@@ -0,0 +1,12 @@
+package WeekOne;
+
+public class SettingUp {
+
+	public static void main (String[] args) {
+		
+		System.out.print("I hope that this can go into a");
+		System.out.print(" repository.");
+		
+	}
+	
+}
---
+++ DataStructures/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Christian
+ *
+ */
+module DataStructures {
+}
